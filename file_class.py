cd628af57cdcb72b57eff6ae5f26db8ce10b6eb2
Leon0241 <enliang.leon.lee@gmail.com>
2023-04-16 05:46:55 +0100

    well documented code

--- file_class.py
+++ file_class.py
@@ -2,21 +2,42 @@ import regex as re
 
 
 class File:
+    """Represents a markdown file and related information.
+
+    Contains information about the lines of where/when links, embeds appear
+    so it will be able to be cross referenced when parsing embeds.
+
+    Attributes:
+        title: The title of the document
+        path: File path to the document fron root
+        contents: Contents of the file. Stored line by line in a list
+        tags: List of tags in the document header
+        contains_frontmatter: Whether pre-existing frontmatter is there
+        header_sections: List of headers and header sections in the file
+        block_sections: List of blocks and block sections in the file
+        embeds: List of links/embeds and related information in the file
+        checkpoints: Dict of important line numbers
+    """
+
     def __init__(self, title, path):
+        """Initialises the class based on the title and file path
+
+        Args:
+            title: The title of the document
+            path: File path to the document from root
+        """
+
         self.title = title
+
         self.path = path
+        # opens file for reading
         tmpfile = open(self.path, "r").readlines()
         self.contents = tmpfile
+        # every line has a \n, except the last one
         for i in range(len(tmpfile)-1):
             line = tmpfile[i]
-            # print(line == "\n")
+            # if the line is blank then leave it
             self.contents[i] = line[:-1] if line != "\n" else line
-
-        # print("--------------")
-        # print(self.contents)
-        # for i in range(len(self.contents)):
-        #     print(self.contents[i])
-        # self.contents = open(self.path, "r").readlines()
 
         self.tags = []
 
@@ -24,39 +45,36 @@ class File:
 
         self.header_sections = []
         self.block_sections = []
+        self.links = []
 
         self.checkpoints = {
             "frontmatter_end": 0,
             "tag_line": 0,
-            "embeds": [],
         }
 
     def __str__(self):
         return self.title
 
     def __repr__(self):
-        # return f'({self.title}, {self.path}, {self.tags})'
-        return str(self.checkpoints["embeds"])
-
-    def get_title(self):
-        return self.title
+        return f'({self.title}, {self.path}, {self.tags})'
+        # return str(self.checkpoints["embeds"])
 
     def find_frontmatter(self):
-        '''
-        Checks if there is frontmatter (so it will be replaced)
+        """Checks if there is frontmatter. Gets end line of pre-existing frontmatter if so.
 
-        Flags self.contains_frontmatter if it does
-        Sets a checkpoint "frontmatter_end"
-        '''
+        Args:
+            None
+        Returns:
+            self.checkpoints["frontmatter_end"]: updated to the end line
+        """
 
         if (self.contents[0][:3] == "\'\'\'"):
             self.contains_frontmatter = True
-            print("true")
 
             # get index of frontmatter end clause
             end_index = 1
             while True:
-                # if it's a frontmatter, else go next line until
+                # if it's a frontmatter, else go next line
                 if (self.contents[end_index][:3] == "\'\'\'"):
                     break
                 else:
@@ -64,11 +82,18 @@ class File:
             self.checkpoints["frontmatter_end"] = end_index
 
     def find_tags(self, TAG_STRING):
-        '''
-        Finds the line that a tag occured in. This should only run if
-        check for tags is true in the config.
-        Returns an exception if a tag is not found
-        '''
+        """Finds if tags are present, and if so what line they are on. Should only run if checks for tags is true in config. 
+
+        Args:
+            TAG_STRING: Custom tag key phrase to search for
+
+        Updates:
+            self.tags: List of tag names
+            self.checkpoints["tag_line"]: line that the tags are on
+        
+        Raises:
+            Prints a warning if no tags are found
+        """
 
         # Searches first 10 lines, could be more but i'd imagine tags are at top
         # min to not overflow if there's less than 10 lines in a file
@@ -88,33 +113,33 @@ class File:
             print("Warning: Tag was not found :/")
             return
 
-    # def look_for_pattern(self, REGEX_PATTERN, current_index):
-    #     search_pattern = None
-
-    #     eof_index = len(self.contents) - 1
-
-    #     while (search_pattern == None) and (current_index < eof_index):
-    #         current_index += 1
-    #         search_pattern = re.search(
-    #             REGEX_PATTERN, self.contents[current_index])
-    #     return current_index
-
     def look_for_headers(self, line, current_index):
-        # "Non capture lookbehind between 1 and 6 '#' (indicates header)"
-        # captures everything after that
-        REGEX_HEADER = "^\#{1,6}\s.*"
-        REGEX_HEADER_PRIO = "\#{1,6}"
+        """Finds header tags in a line, and the block it represents in the text.
+
+        Args:
+            line: File contents to parse
+            current_index: Line number to start from
+
+        Returns:
+            self.header_sections: Creates a header reference. Appends list of type:
+            [start, end, ref]: start line, end line, header name
+        """
+
+        REGEX_HEADER = "^\#{1,6}\s.*"   # 1-6 "# [some text]"
+        REGEX_HEADER_PRIO = "\#{1,6}"   # 1-6 "#"
+
         # does the line have a header or not
         does_line_have_header = re.search(REGEX_HEADER, line)
         # if it does have a header
         if does_line_have_header != None:
             header_title = does_line_have_header.group()
-            title_hashes = re.search(REGEX_HEADER_PRIO, header_title).group()
-            highest_header = len(title_hashes)
-            # print("full header: " + header_title)
-            # print("highest header: " + str(highest_header))
+            
+            # How many hashtags are in the header
+            highest_header = len(
+                re.search(REGEX_HEADER_PRIO, header_title).group())
+            # Offset the hashtags
             header_title = header_title[highest_header:]
-            # print("altered title: " + header_title)
+
             # set up variables for dictionary
             header_start = current_index
             # set up variables for loop
@@ -122,12 +147,13 @@ class File:
             header_end = current_index
             eof_index = len(self.contents) - 1
 
-            # while there isn't a next header, and
+            # search down through each line until finds header/eof
             while (search_result == None) and (header_end < eof_index):
                 header_end += 1
-                # print("line: " + self.contents[header_end])
+                # search for header
                 search_result = re.search(
                     REGEX_HEADER, self.contents[header_end])
+                # if header is found, check the header isn't lower ranked
                 if search_result != None:
                     reg = re.search(REGEX_HEADER_PRIO, search_result.group())
                     if reg != None and len(reg.group()) > highest_header:
@@ -141,29 +167,37 @@ class File:
                 "end": header_end,
                 "ref": header_title
             })
-            # print("----------")
-            # print("HEADERS")
-            # print(self.header_sections)
-            # print("----------")
         else:
             return
 
     def look_for_blocks(self, line, current_index):
-        # "Non capture lookbehind between 1 and 6 '#' (indicates header)"
-        # captures everything after that
-        REGEX_BLOCK = "^\^.{1,6}$"
-        REGEX_EMPTY = "^\s{0,}$"
-        # does the line have a header or not
+        """Finds block tags in a line, and the block it represents in the text.
+
+        Args:
+            line: File contents to parse
+            current_index: Line number to start from
+
+        Updates:
+            self.block_sections: Creates a block refernece. Appends list of type:
+            [start, end, ref]: start line, end line, header name
+        """
+
+        REGEX_BLOCK = "^\^.{6}$"  # "^"" followed by 6 characters
+        REGEX_EMPTY = "^\s{0,}$"    # Empty line
+        
+        # does the line have a header or not, and if yes
         does_line_have_block = re.search(REGEX_BLOCK, line)
-        # if it does have a header
         if does_line_have_block != None:
             # set starting line variable
             block_end = current_index
+            # title is the reference e.g. "^10256a"
             block_title = does_line_have_block.group()
-            # loop continuously
+            
+            # set up variables for loop
             search_result = None
             block_start = current_index - 1
-            # print(block_start)
+
+            # search up through each line until finds an empty line
             while (search_result == None) and (block_start > 0):
                 block_start -= 1
                 search_result = re.search(
@@ -174,44 +208,55 @@ class File:
                 "end": block_end - 1,
                 "ref": block_title
             })
-            # print("----------")
-            # print("BLOCKS")
-            # print(self.block_sections)
-            # print("----------")
         else:
             return
 
     def get_section_contents(self):
+        # wip
         for section in self.header_sections:
-            print("printing a section")
             subsection = self.contents[section["start"]:section["end"]]
-            for i in subsection:
-                print(i)
-            # for i in range(section["start"], section["end"]):
-            #     print(self.contents[i])
 
     def look_for_links(self, line, current_index):
-        REGEX_LINK = "\[\[.*\]\]"
-        REGEX_EMBED = "(?<=!\[\[).*(?=\]\])"
-        REGEX_BLOCK = "#\^.{1,6}$"
-        REGEX_HEADER = "#"  # lol
-        REGEX_ALT = "\|"
-        REGEX_IMAGE = "\.(png|jpg|svg|webp)"
+        """
+        Finds links in a line. Gets type, whether it is an embed, and whether it has alt text.
 
+        Args:
+            line: File contents to parse
+            current_index: Line number to start from
+
+        Updates:
+            self.links: Update a link reference. Appends dict of type:
+            {
+                line: index of line
+                type: can be "block", "header", "image", "page"
+                label: whether there is alt text
+                text: contents of the link
+            }
+        """
+
+        REGEX_LINK = "\[\[.*\]\]"               # "[[" + some text + "]]"
+        REGEX_EMBED = "(?<=!\[\[).*(?=\]\])"    # "![[" text "]]" (get text)
+        REGEX_BLOCK = "#\^.{6}$"                # "^" followed by 6 chars
+        REGEX_HEADER = "#"                      # "#" lol
+        REGEX_ALT = "\|"                        # "|" lol 2 
+        REGEX_IMAGE = "\.(png|jpg|svg|webp)"    # . followed by a file ext.
+
+        # does line have a header, and if it does
         does_line_have_link = re.findall(REGEX_LINK, line)
-        # if it does have a header
         if does_line_have_link != None:
             for link in does_line_have_link:
-                # flags
-                # check if file is embed or not
+                # big flag chain
+                # check if link is an embed or not
                 is_embed = False
                 does_line_have_embed = re.search(REGEX_EMBED, link)
+                # if yea then raw text is the regex, otherwise strip the [[]]
                 if does_line_have_embed != None:
                     is_embed = True
                     raw_text = does_line_have_embed.group()
                 else:
                     raw_text = link[2:-2]
 
+                # check link type (block/header/image/full page)
                 link_type = ""
                 if re.search(REGEX_BLOCK, raw_text) != None:
                     link_type = "block"
@@ -222,10 +267,12 @@ class File:
                 else:
                     link_type = "page"
 
+                # check if link has alt text or not
                 label = False
                 if re.search(REGEX_ALT, raw_text) != None:
                     label = True
-                self.checkpoints["embeds"].append({
+                
+                self.links.append({
                     "line": current_index,
                     "type": link_type,
                     "label": label,
@@ -235,12 +282,22 @@ class File:
             return
 
     def the_big_sweeper(self):
-        # print("--------------")
-        # print("Title: " + self.title)
-        # print(self.contents[0][-2])
+        '''Iterate over every line finding headers, blocks, and links
+
+        Args:
+            None
+        Updates:
+            self.header_sections: headers and the sections they represents
+            self.block_sections: blocks and the sections they represents
+            self.links: link types and whether it is an embed or has alt text
+        '''
+
+
         for i, line in enumerate(self.contents):
-            # continue
             self.look_for_headers(line, i)
             self.look_for_blocks(line, i)
             self.look_for_links(line, i)
         self.get_section_contents()
+
+
+print(help(File.look_for_links))
